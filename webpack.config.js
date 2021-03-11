9250dc7fee9010864bd4934aa1dd76b5c5e48d61
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-03-11 16:01:15 +0600

    webpack & manifest update

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,17 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: {
-    background: ['babel-polyfill', './src/background.js'],
-    main: './src/medium-unlocker.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[name]_[local]_[hash:base64]',
-              sourceMap: true,
-              minimize: true,
-            },
-          },
-        ],
-      },
+    entry: {
+        background: [
+            'babel-polyfill',
+            './src/background.js'
+        ], content_script: './src/content_script.js',
+    }, output: {
+        filename: '[name].bundle.js',
+    }, plugins: [
+        new CopyWebpackPlugin([
+            {from: 'static', to: 'static'},
+            {from: 'manifest.json', to: 'manifest.json'},
+        ], {debug: true, context: '.'}),
     ],
-  },
-  plugins: [
-    new CopyWebpackPlugin(
-      [
-        {from: 'static', to: 'static'},
-        {from: 'src/html', to: 'html'},
-        {from: 'manifest.json', to: 'manifest.json'},
-      ],
-      {debug: true, context: '.'}
-    ),
-  ],
 };
