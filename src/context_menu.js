5dc42b640eb7a806dcc934330b171f1d9392d999
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-03-25 08:29:39 +0600

    context menu created on remove all when background page is reloaded

--- src/context_menu.js
+++ src/context_menu.js
@@ -24,38 +24,48 @@ export default function () {
         });
     }
 
-    /**
-     * Parent context menu
-     */
-    browserNamespace().contextMenus.create({
-        title: "Unlock this article",
-        id: 'medium-unlock-parent-context-menu',
-        contexts: ["all"],
-        documentUrlPatterns: CONFIGURATION.url_patterns,
-    });
-
-    /**
-     * Child Context menu - New tab
-     */
-    browserNamespace().contextMenus.create({
-        'title': 'Anonymously in new tab',
-        'parentId': 'medium-unlock-parent-context-menu',
-        contexts: ["all"],
-        onclick: contextMenuAnonymous,
-    });
-
-    /**
-     * Child context menu - Debugger
-     * Adds only if the debugger is available
-     */
-    if ( browserNamespace().debugger ) {
+    function createContextMenus () {
+        /**
+         * Parent context menu
+         */
         browserNamespace().contextMenus.create({
-            'title': 'With debugger',
-            'parentId': 'medium-unlock-parent-context-menu',
+            title: "Unlock this article",
+            id: 'medium-unlock-parent-context-menu',
             contexts: ["all"],
-            onclick: contextMenuDebugger,
+            documentUrlPatterns: CONFIGURATION.url_patterns,
         });
+
+        /**
+         * Child Context menu - New tab
+         */
+        browserNamespace().contextMenus.create({
+            title: 'Anonymously in new tab',
+            parentId: 'medium-unlock-parent-context-menu',
+            id: 'mu-context-anon',
+            contexts: ["all"],
+            onclick: contextMenuAnonymous,
+        });
+
+        /**
+         * Child context menu - Debugger
+         * Adds only if the debugger is available
+         */
+        if ( browserNamespace().debugger ) {
+            browserNamespace().contextMenus.create({
+                title: 'With debugger',
+                parentId: 'medium-unlock-parent-context-menu',
+                id: 'mu-context-debugger',
+                contexts: ["all"],
+                onclick: contextMenuDebugger,
+            });
+        }
     }
+
+    /**
+     * Unchecked runtime.lastError: Cannot create item with duplicate id
+     * https://stackoverflow.com/a/37000388/2190689
+     */
+    browserNamespace().contextMenus.removeAll(createContextMenus);
 
     return Promise.resolve();
 }
